89c84875f64c688e4a367ecf0b517e9c38e55bba
jagannath-mobifyi <jagannath@mobifyi.com>
2022-06-14 12:45:49 +0530

    make it better

--- hoc/Wrapper.tsx
+++ hoc/Wrapper.tsx
@@ -1,5 +1,5 @@
 // This function is used to wrap the children into one,
 // use this as a wrapper in your components and containers
 
-const Wrapper = (props) => props.children;
+const Wrapper = (props:any) => props.children;
 export default Wrapper;
